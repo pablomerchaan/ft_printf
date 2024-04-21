d4f406f0075299cc9212a8c3c999d213e1f05fa1
Rosario Arnau <rosario@iMac-de-Rosario.local>
2024-04-21 14:39:37 +0200

    addputhexa

--- ft_printf.c
+++ ft_printf.c
@@ -1,4 +1,16 @@
-#include "printflib"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paperez- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/20 17:41:17 by paperez-          #+#    #+#             */
+/*   Updated: 2024/03/20 17:49:04 by paperez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "printflib.h"
 
 int	ft_printf(char *str, ...)
 {
@@ -9,10 +21,10 @@ int	ft_printf(char *str, ...)
 	size = 0;
 	c = 0;
 	va_start(args, str);
-	while (str[c] != '\0)
+	while (str[c] != '\0')
 	{
-		if (str[c] == %)
-			size += ft_printp(args, str[c + 1])
+		if (str[c] == "%")
+			size += ft_printp(args, str[c + 1]);
 		else
 		{
 			ft_putchar(str[c]);
@@ -26,16 +38,19 @@ int	ft_printf(char *str, ...)
 
 int	ft_printp(va_list args, char c)
 {
+	int	size;
+
+	size = 0;
 	if (c == s)
-		size += ft_putstr(va_arg (args, char *);
+		size += ft_putstr(va_arg (args, char *));
 	else if (c == i || c == d)
-		size += ft_putnbr(va_arg (args, int);
+		size += ft_putnbr(va_arg (args, int));
 	else if (c == c)
-		size *= ft_putchar(va_arg (args, char);
+		size *= ft_putchar(va_arg (args, char));
 	else if (c == p)
-		size += ft_putpointer(va_arg (args, unsigned long long);
+		size += ft_putpointer(va_arg (args, unsigned long long));
 	else if (c == u)
-		size += ft_putunsig(va_arg (args, unsigned int);
+		size += ft_putunsig(va_arg (args, unsigned int));
 	else if (c == x || c == X)
 		size += ft_puthexa(va__arg (args, int), c);
 	else
---
+++ ft_puthexa.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paperez- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/20 17:58:04 by paperez-          #+#    #+#             */
+/*   Updated: 2024/03/25 18:54:54 by paperez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+static char	maphexa(unsigned int h, char c)
+{
+	char	res;
+
+	if (h > 9 && c == 'x')
+		res = (h - 10 + 'a');
+	else if (h > 9 && c == 'X')
+		res = (h - 10 + 'A');
+	else
+		res = h + '0';
+	return (res);
+}
+
+static int	initcount(int count)
+{
+	if (count == 0)
+	{
+		write (1, "0x", 2);
+		count += 2;
+	}
+	return (count);
+}
+
+static int	puthexa(unsigned int h, const char c, int count)
+{
+	unsigned int	h2;
+	char	s;
+	
+	count = initcount(count);
+	if (h < 16)
+	{
+		s = maphexa(h, c);
+		write (1, &s, 1);
+		count++;
+		return (count);
+	}
+	if (h >= 16)
+	{
+		h2 = h / 16;
+		count = puthexa(h2, c, count);
+		s = maphexa(h % 16, c);
+		write (1, &s, 1);
+		count++;
+	}
+	return (count);
+}
+
+int	main()
+{
+	int c;
+	c = puthexa(0, 'X', 0);
+	printf("%i", c);
+	return (0);
+}
